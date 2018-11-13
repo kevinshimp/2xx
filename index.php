4484d282c47c66df3adb82d1021664a256c5ec27
kevinshimp <ks95307@ravens.racc.edu>
2018-11-12 22:38:31 -0500

    updated config, content, and functions data

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
     require_once('include/header.inc.php');
- ?>
-      <section>
-        <p>this is where the page content goes</p>
-      </section>
 
+      getPage($content['pages']);
 
+?>
 <?php
 
 require_once('include/footer.inc.php');
 
 ?>
-    
